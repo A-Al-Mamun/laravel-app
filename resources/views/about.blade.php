f1721066464d51ab45b6b6b9169a5d361ea79563
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2023-12-13 21:25:01 +0600

    Laravel app project upload

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About page of Laravel App</title>
+</head>
+<body>
+    <h1>About section Page</h1>
+</body>
+</html>
